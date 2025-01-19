4180ac07d761b56ad12ea864906978d34b3e03de
web3-dar <Onlineagentalmashell@gmail.com>
2025-01-19 13:31:38 +0100

    change been made

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-const GetStarted = ({arrow}) => {
+const GetStarted = ({ arrow }) => {
+  const [ref, inView] = useInView({ triggerOnce: true });
   return (
-    <div className='relative  md:hidden flex justify-center'>
-      <div className='border-4 border-[#33bbcf] w-[200px] h-[200px] flex flex-col justify-center items-center rounded-full'>
-        <p className='flex items-center text-[30px] text-gradient font-semibold'>Get <img className='object-contain w-[30px] h-[30px]' src={arrow} alt="" /></p>
-        <p className='text-[30px] text-gradient font-semibold'>Started</p>
+    <motion.div
+      ref={ref}
+      initial={{ y: 100 }}
+      animate={inView ? { y: 0 } : {}}
+      transition={{ duration: 0.3 }}
+      className="relative  md:hidden flex justify-center"
+    >
+      <div className="border-4 border-[#33bbcf] w-[200px] h-[200px] flex flex-col justify-center items-center rounded-full">
+        <p className="flex items-center text-[30px] text-gradient font-semibold">
+          Get{" "}
+          <img
+            className="object-contain w-[30px] h-[30px]"
+            src={arrow}
+            alt=""
+          />
+        </p>
+        <p className="text-[30px] text-gradient font-semibold">Started</p>
       </div>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
-export default GetStarted
+export default GetStarted;
