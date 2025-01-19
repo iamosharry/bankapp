884d7ba55a33b570256bbf4112cb0eaa3d03349c
web3-dar <Onlineagentalmashell@gmail.com>
2025-01-19 14:59:22 +0100

    change been made

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-
-const Card = ({card}) => {
+const Card = ({ card }) => {
+  const [ref, inView] = useInView({ triggerOnce: true, threshold: 1 });
   return (
-    <div className='mt-[100px] px-7 md:flex items-center md:space-x-6'>
-      <div className='md:w-[50%] sm:w-[50%]'>
-        <h2 className='text-[30px] font-semibold md:max-w-[500px]'>Find a better card deal in few easy steps.</h2>
-        <p className='mt-7 md:max-w-[500px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloribus nesciunt autem reprehenderit culpa, repellat quaerat facilis reiciendis rerum.</p>
-        <div className='mt-7'>
-          <button className='bg-blue-gradient text-blue-950 py-5 w-full md:max-w-[200px] rounded-lg text-[20px] font-semibold'>Get Started</button>
+    <div className="mt-[100px] px-7 md:flex items-center md:space-x-6">
+      <div className="md:w-[50%] sm:w-[50%]">
+        <h2 className="text-[30px] font-semibold md:max-w-[500px]">
+          Find a better card deal in few easy steps.
+        </h2>
+        <p className="mt-7 md:max-w-[500px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+          doloribus nesciunt autem reprehenderit culpa, repellat quaerat facilis
+          reiciendis rerum.
+        </p>
+        <div className="mt-7">
+          <button className="bg-blue-gradient text-blue-950 py-5 w-full md:max-w-[200px] rounded-lg text-[20px] font-semibold">
+            Get Started
+          </button>
         </div>
       </div>
-      <div className='flex justify-center mt-[100px] md:mt-0 md:w-[50%] sm:w-[50%]'>
-        <img className='object-contain w-[350px] sm:w-[600px]' src={card} alt="" />
-      </div>
+      <motion.div
+        ref={ref}
+        initial={{ y: 50, opacity: 0 }}
+        animate={inView ? { y: 0, opacity: 1 } : {}}
+        transition={{ duration: 0.5 }}
+        className="flex justify-center mt-[100px] md:mt-0 md:w-[50%] sm:w-[50%] "
+      >
+        <img
+          className="object-contain w-[350px] sm:w-[600px]"
+          src={card}
+          alt=""
+        />
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,21 +1,55 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-const Transaction = ({bill,apple,google}) => {
+const Transaction = ({ bill, apple, google }) => {
+  const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.15 });
+  const [ref2, inView2] = useInView({ triggerOnce: true, threshold: 0.15 });
   return (
-    <div className='md:flex items-center md:justify-center'>
-      <div className='flex md:w-[50%] sm:justify-start justify-center mt-[80px] px-3'>
-        <img className='w-[350px] md:w-[450px] lg:w-[500px] object-contain ' src={bill} alt="bill-icon" />
-      </div>
-      <div className='px-5 mt-10 md:w-[50%]'>
-        <h3 className='font-semibold text-[25px] md:text-[30px] lg:text-[50px] max-w-[350px] lg:max-w-[600px]'>Easily control your billing and invoicing.</h3>
-        <p className='mt-7'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi facere dignissimos impedit quidem perferendis amet dolores quae quisquam, accusamus sint.</p>
-        <div className='flex mt-7 items-center justify-between sm:justify-start sm:space-x-8 md:justify-start md:space-x-8'>
-          <img className='w-[150px] object-contain' src={apple} alt="apple-icon" />
-          <img className='w-[150px] object-contain' src={google} alt="google-icon" />
+    <div className="md:flex items-center md:justify-center mt-10">
+      <motion.div
+        ref={ref}
+        initial={{ x: -300, opacity: 0 }}
+        animate={inView ? { x: 15, opacity: 1 } : {}}
+        transition={{ duration: 0.7 }}
+        className="flex md:w-[50%] sm:justify-start justify-center  px-3"
+      >
+        <img
+          className="w-[350px] md:w-[450px] lg:w-[500px] object-contain "
+          src={bill}
+          alt="bill-icon"
+        />
+      </motion.div>
+      <motion.div
+        ref={ref2}
+        initial={{ y: 50, opacity: 0 }}
+        animate={inView2 ? { y: 0, opacity: 1 } : {}}
+        transition={{ delay: 0.7, duration: 0.7 }}
+        className="px-5 mt-10 md:w-[50%]"
+      >
+        <h3 className="font-semibold text-[25px] md:text-[30px] lg:text-[50px] max-w-[350px] lg:max-w-[600px]">
+          Easily control your billing and invoicing.
+        </h3>
+        <p className="mt-7">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi facere
+          dignissimos impedit quidem perferendis amet dolores quae quisquam,
+          accusamus sint.
+        </p>
+        <div className="flex mt-7 items-center justify-between sm:justify-start sm:space-x-8 md:justify-start md:space-x-8">
+          <img
+            className="w-[150px] object-contain"
+            src={apple}
+            alt="apple-icon"
+          />
+          <img
+            className="w-[150px] object-contain"
+            src={google}
+            alt="google-icon"
+          />
         </div>
-      </div>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Transaction
+export default Transaction;
